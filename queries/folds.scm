770babd7bdfae078d46d7d8bdb328a1cf63aaf35
Vsevolod Donchenko <vsevolond@MacBook-Pro-Vsevolod.local>
2026-02-14 16:15:17 +0300

    added fold support for pageflow language

---
+++ queries/folds.scm
@@ -0,0 +1,27 @@
+[
+  (newpage_block)
+  (section_block)
+  (subsection_block)
+  (section_newpage_block)
+  
+] @fold
+
+[
+  (vstack_block)
+  (hstack_block)
+  (zstack_block)
+  
+] @fold
+
+[
+  (text_block)
+  (math_block)
+  
+] @fold
+
+[
+  (image_element)
+  (divider_element)
+  (listing_element)
+  
+] @fold
